ce02e0ec1563e96544c323ca1b6bd97dd4e7be1b
Emile <emile@MacBook-Air-de-Emile.local>
2026-02-03 17:00:59 +0100

    tentative de MaxPoolingAgg

--- src/layers.py
+++ src/layers.py
@@ -1,19 +1,25 @@
 import torch
 import torch.nn as nn
 
-import torch
-import torch.nn as nn
-
 class MeanAggregator(nn.Module):
     def __init__(self):
         super(MeanAggregator, self).__init__()
 
-    def forward(self, neighbors_features):
+    def forward(self, neighbours_features):
         """
         Args:
             neighbors_features (torch.Tensor): Tensor de forme (batch_size, num_neighbors, feature_dim)
 
         Returns:
-            torch.Tensor: Tensor de forme (batch_size, feature_dim)
+            torch.Tensor: Tensor de forme (batch_size, feature_dim), moyenne element wise
         """
-        return torch.mean(neighbors_features, dim=1)
+        return torch.mean(neighbours_features, dim=1)
+    
+class MaxPoolingAggregator(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(MaxPoolingAggregator, self).__init__()
+        self.fc = nn.Linear(in_features, out_features)
+
+    def forward(self, neighbours_features):
+        out = nn.ReLU(self.fc(neighbours_features))
+        return torch.max(out, dim=1)
