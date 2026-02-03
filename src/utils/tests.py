20241d76e3b0fd2cf2b32e84d8a0924b35f98119
Emile <emile@MacBook-Air-de-Emile.local>
2026-02-03 15:05:42 +0100

    neighbours sampling

---
+++ src/utils/tests.py
@@ -0,0 +1,26 @@
+import numpy as np
+import random
+from utils import get_n_neighbours
+
+#Features
+features = np.array([
+    [1.0, 0.1, 5.0],  # Nœud 0
+    [0.2, 0.8, 1.0],  # Nœud 1
+    [1.5, 0.3, 4.0],  # Nœud 2
+    [0.1, 0.9, 0.0],  # Nœud 3
+    [1.2, 0.2, 4.5],  # Nœud 4
+    [0.0, 0.7, 0.5]   # Nœud 5
+], dtype=np.float32)
+
+#Adjacency list
+adj = {
+    0: [1, 2, 4],
+    1: [0, 3, 5],
+    2: [0, 4],
+    3: [1, 5],
+    4: [0, 2],
+    5: [1, 3]
+}
+
+neighbours = get_n_neighbours(adj, 0, 2)
+print("2 Neighbors of node 0:", neighbours)
---
+++ src/utils/utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def get_n_neighbours(graph, node, n):
+    """
+    Retourne n voisins aléatoires d'un noeud donné dans un graphe représenté par une liste d'adjacence.
+
+    Parameters:
+    graph (dict): Dictionnaire représentant la liste d'adjacence du graphe.
+    node: Le noeud dont on veut obtenir les voisins.
+    n (int): Le nombre de voisins à retourner.
+
+    Returns:
+    list: Une liste des n voisins aléatoires du noeud spécifié.
+    """
+    if node not in graph:
+        return []
+
+    neighbors = graph[node]
+    if len(neighbors) <= n:
+        return neighbors
+    return list(np.random.choice(neighbors, n, replace=False))
+
