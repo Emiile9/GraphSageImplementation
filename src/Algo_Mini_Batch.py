dda9457dfcd553973f5df27cdfca2f84325409c8
Tomil <68993838+93kazy@users.noreply.github.com>
2026-02-09 21:02:12 +0100

    Update Algo_Mini_Batch.py

--- src/Algo_Mini_Batch.py
+++ src/Algo_Mini_Batch.py
@@ -1,50 +1,69 @@
+"""
+Fichier contenant l'algorithme forward propagation mini batch
+"""
+
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import networkx as nx
+from src.utils.utils import get_n_neighbours,get_n_features
+from typing import List,Callable
 
 class AlgoMiniBatch(nn.Module):
-    def __init__(self,depth,weight_matrices,sigma,agg_f):
+    def __init__(self,depth:int,weight_matrices:nn.ModuleList,sigma:Callable,agg_f:nn.ModuleList):
+        """
+        Args :
+            depth (int) : Nombre de couches
+            weight_matrices (nn.ModuleList) : W^k, liste des matrices de poids pour chaque couche
+            sigma (Callable) : Fonction d'activation non linéaire 
+            agg_f (nn.ModuleList) : Liste des fonctions d'agrégation pour chaque couche
+        """
         super(AlgoMiniBatch,self).__init__()
         self.K = depth
         self.W = weight_matrices
         self.sigma = sigma
-        self.agg_f = agg_f
+        self.agg_f = agg_f  
+    
+    def forward_propagation(self,G:nx.Graph,nodes,sampling_size:List[int])-> torch.Tensor:
+        """
+        Génère les embeddings pour un mini batch de noeuds
 
-    def sampling(self,nodes,neighbor,sampling_size):
-        B = [set() for i in range(self.K+1)]
-        B[self.K] = set(nodes)
+        Args:
+            G (nx.Graph) : Graphe concerné
+            nodes : B, Batch de noeuds cibles
+            sampling_size (List[int]) : Taille du sampling pour chaque couche
+
+        Returns: 
+            z (torch.Tensor) : Embeddings pour le mini batch
+        """
+
+
+        B = {self.K: set(nodes)}
 
         for k in range(self.K,0,-1):
-            B[k-1] = B[k].copy()
+            B[k-1] = B.copy()
             for u in B[k]:
-                neighbors = neighbor.get(u,[])
-                sampling_size = sampling_size[k-1]
-                sampled_neighbor = np.random.choice(neighbors,size=sampling_size,replace=True)
-                B[k-1] = B[k-1].union(set(sampled_neighbor))
-        return B
-    
-    def forward_propagation(self,nodes,neighbor,feature,sampling_size):
-        B = self.sampling(nodes,neighbor,sampling_size)
+                neighbors = get_n_neighbours(G,u,sampling_size[k-1])
+                B[k-1] = B[k-1].union(set(neighbors))
+
         h = {}
-        h[0] = {node:feature[node] for node in B[0]}
+        h[0] = {node:torch.tensor(get_n_features(G,node)) for node in B[0]}
 
         for k in range(self.K):
             current_h = {}
             for u in B[k+1]:
-                neighbors = neighbor.get(u,[])
-                sampling_size = sampling_size[k]
-                sampled_neighbor = np.random.choice(neighbors,size=sampling_size,replace=True)
-                h_k_1 = [h[k][v] for v in sampled_neighbor]
-                h_k_N = self.agg_f[k](h_k_1)
+                neighbors = get_n_neighbours(G,u,sampling_size[k])
+                h_k_1 = [h[k][v] for v in neighbors]
+                h_k_N = self.agg_f[k].aggregate(h_k_1)
                 concat = torch.cat((h[k][u],h_k_N),dim=0)
                 h_u_k = self.sigma(self.W[k](concat))
 
-                if(F.normalize(h_u_k,p=2,dim=0)!=0):
-                    h_u_k = h_u_k/F.normalize(h_u_k,p=2,dim=0)
+                norm = torch.norm(h_u_k,p=2)
+                if(norm!=0):
+                    h_u_k = h_u_k/norm
                 current_h[u] = h_u_k
             h[k+1] = current_h
         z = torch.stack([h[self.K][u] for u in nodes])
         return z
-            
-
